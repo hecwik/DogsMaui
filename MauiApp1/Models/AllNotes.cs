34f807158a5702a37769c21e3d123cefe82db7c9
Hector <56518744+hecwik@users.noreply.github.com>
2023-04-25 00:04:44 +0200

    Added more models and views

---
+++ MauiApp1/Models/AllNotes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1.Models;
+
+internal class AllNotes
+{
+    public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+    public AllNotes() =>
+        LoadNotes();
+
+    public void LoadNotes()
+    {
+        Notes.Clear();
+
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        IEnumerable<Note> notes = Directory
+            .EnumerateFiles(appDataPath, "*.notes.txt")
+            .Select(fileName => new Note()
+            {
+                Filename = fileName,
+                Text = File.ReadAllText(fileName),
+                Date = File.GetCreationTime(fileName)
+            })
+            .OrderBy(note => note.Date);
+
+        foreach(Note note in notes)
+        {
+            Notes.Add(note);
+        }
+
+    }
+
+
+}
